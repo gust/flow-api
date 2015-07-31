a07ef56f705b2289cc176007f54a4236d773e163
James Vanneman and Peter Swan <zephyr-eng+james+pdswan@gust.com>
2015-07-31 17:42:31 -0400

    aint working

--- Main.hs
+++ Main.hs
@@ -3,12 +3,14 @@
 import Migrations
 import qualified Web.Scotty as WS
 import Control.Monad.Trans
+import Control.Monad.State.Class
 import TrackerTagging
 import PivotalTracker
 import Database.Persist
 import System.Environment(getEnv)
 import Control.Monad(liftM)
 import Database.Persist.Postgresql
+import qualified Data.ByteString.Char8 as BCH
 import Control.Monad.Logger
 
 
@@ -17,11 +19,19 @@ connStr = "host=localhost dbname=flow_api user=gust port=5432"
 
 runDbIO statement = runStdoutLoggingT $ withPostgresqlConn connStr $ \connection -> do
           liftIO $ runSqlPersistM statement connection
+data Environment = Environment {
+    httpPort :: Int
+  , pivotalTrackerApiToken :: BCH.ByteString
+}
+
 main :: IO ()
 main =  do
+  apiToken <- getApiToken
   runDbIO $ runMigrationUnsafe migrateAll
   port <- liftM read $ getEnv "PORT"
   WS.scotty port $ do
+    let environment = Environment port apiToken
+    put environment
     WS.post "/" $ do
        gitLog <- WS.param "git_log"
        app    <- WS.param "app"
